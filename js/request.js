b0feeead4045e2fb616459fc90b1da58cb07a141
Hoang Do <hoangdo.dh94@gmail.com>
2016-03-30 13:34:33 +0700

    init

---
+++ js/request.js
@@ -0,0 +1,42 @@
+function authenticate(credentials, callback) {
+  var settings = {
+    "url": "http://api.ws.hoangdo.info/auth/",
+    "method": "POST",
+    "headers": {
+      "authorization": "Basic " + btoa(credentials.username + ':' + credentials.password),
+    },
+  };
+  $.ajax(settings).done(function(response) {
+    if (response.status === 1) {
+      callback(response.data);
+    } else {
+      callback(null);
+    }
+  });
+}
+
+function getUsers(callback) {
+
+}
+
+function getUser(userId, callback) {
+
+}
+
+function updateInfo(userId, newInfo, callback) {
+
+}
+
+function getBooks(callback) {
+  var settings = {
+    "url": "http://api.ws.hoangdo.info/books/",
+    "method": "GET",
+  };
+  $.ajax(settings).done(function(response) {
+    if (response.status === 1) {
+      callback(response.data);
+    } else {
+      callback(null);
+    }
+  });
+}
