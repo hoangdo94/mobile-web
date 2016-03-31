bab360e72c87ec67ee3242f2247f68ab6d81dcc9
Hoang Do <hoangdo.dh94@gmail.com>
2016-03-31 15:19:26 +0700

    update

--- js/vendor/request.js
+++ js/vendor/request.js
@@ -25,13 +25,13 @@ function register(info, callback) {
     "method": "POST",
     "data": JSON.stringify(info),
     "contentType": "application/json",
-  }
+  };
   $.ajax(settings)
       .done(function (response) {
         if (response.status === 1)
           callback(response);
         else callback(null);
-      })
+      });
 
 }
 
@@ -63,12 +63,12 @@ function updateInfo(userId, header, newInfo, callback) {
     },
     contentType: "application/json",
     success: function (res) {
-      callback(res, 1)
+      callback(res, 1);
     },
     error: function(res) {
-      callback(res, 2)
+      callback(res, 2);
     }
-  })
+  });
 }
 
 function getBooks(callback) {
